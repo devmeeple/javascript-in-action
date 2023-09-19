fdfebc792fcba7bfd5be161c492b2e4405e7bf1a
미플 <devmeeple@gmail.com>
2023-09-19 23:19:32 +0900

    style(stars): 별찍기 예제 2차 업데이트
    상수도입

--- zerocho/drawStars.js
+++ zerocho/drawStars.js
@@ -1,7 +1,11 @@
 /**
- * 렛츠기릿 자바스크립트 별찍기 숙제 1차 답안
+ * 렛츠기릿 자바스크립트 별찍기 숙제 2차 답안
+ * 하드코딩 -> 상수 도입
  */
-for (let i = 0; i < 5; i++) {
+
+const MAX_STARS = 5;
+
+for (let i = 0; i < MAX_STARS; i++) {
     for (let j = 0; j <= i; j++) {
         process.stdout.write('*');
     }
