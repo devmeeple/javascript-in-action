a0358ad654426a1f4ab147a4732f171a8d127167
미플 <devmeeple@gmail.com>
2023-09-20 23:40:25 +0900

    style(stars): 별찍기 예제 3차 업데이트
    피라미드, 역 피라미드 구현

--- zerocho/drawStars.js
+++ zerocho/drawStars.js
@@ -1,12 +1,29 @@
 /**
- * 렛츠기릿 자바스크립트 별찍기 숙제 2차 답안
+ * 렛츠기릿 자바스크립트 별찍기 숙제 3차 답안
  * 하드코딩 -> 상수 도입
  */
 
 const MAX_STARS = 5;
+const MIN_STARS = 0;
 
-for (let i = 0; i < MAX_STARS; i++) {
-    for (let j = 0; j <= i; j++) {
+for (let i = MIN_STARS; i < MAX_STARS; i++) {
+    for (let j = MIN_STARS; j <= i; j++) {
+        process.stdout.write('*');
+    }
+    console.log();
+}
+
+console.log('------ 피라미드 별찍기 -----');
+
+for (let i = MAX_STARS; MIN_STARS < i; i--) {
+    for (let j = MIN_STARS; j < i; j++) {
+        process.stdout.write('*');
+    }
+    console.log();
+}
+
+for (let i = 1; i <= 9; i+2) {
+    for (let j = 1; j <= i; j++) {
         process.stdout.write('*');
     }
     console.log();
