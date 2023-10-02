c7a9295b4863e3eb1c9d966f7c35139ba4d55e2d
미플 <devmeeple@gmail.com>
2023-10-02 10:32:23 +0900

    feat(word-relay): 3장 끝말잇기 순서도 최적화
    코드 중복을 제거

--- zerocho/word-relay.js
+++ zerocho/word-relay.js
@@ -7,37 +7,23 @@ let word; // 제시어
 let newWord; // 새로 입력한 단어
 
 const onClickButton = () => {
-    if (!word) { // 제시어가 비어 있나요?
+    if (!word || word[word.length -1] === newWord[0]) { // 제시어가 비어있나? 제시어 마지막 글자와 입력한 단어 첫 글자 비교
         // 비어있다.
         word = newWord; // 입력한 단어가 제시어가 된다 + 데이터를 바꾸고 화면 출력도 신경써서 코딩한다
         $word.textContent = word;
-        const order = parseInt($order.textContent);
+        const order = parseInt($order.textContent); // 현재 순서
         if (order + 1 > number) {
             $order.textContent = 1;
         } else {
             $order.textContent = order + 1;
         }
-        $input.value = ''; // 입력한 내용을 사용자가 일일히 지우지 않도록 값을 초기화 시켜준다
-        $input.focus();
     } else {
-        // 비어있지 않다. 입력한 단어가 유효한지 검증한다
-        if (word[word.length - 1] === newWord[0]) { // 제시어의 마지막자리 === 입력한단어의 첫 번째 자리
-            word = newWord;
-            $word.textContent = word;
-            const order = parseInt($order.textContent);
-            if (order + 1 > number) {
-                $order.textContent = 1;
-            } else {
-                $order.textContent = order + 1;
-            }
-            $input.value = '';
-            $input.focus();
-        } else {
-            alert('올바르지 않은 단어입니다');
-            $input.value = '';
-            $input.focus();
-        }
+        alert('올바르지 않은 단어입니다');
     }
+    // 사용자 편의를 위해 추가
+    // 입력한 내용을 사용자가 일일히 지우지 않도록 값을 초기화 시켜준다
+    $input.value = '';
+    $input.focus();
 };
 
 const onInput = (event) => {
