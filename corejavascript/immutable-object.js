0d9c7e9d16d5093ac369773c6de2fc02008179fa
미플 <devmeeple@gmail.com>
2023-10-08 22:26:24 +0900

    docs(immutable-object): 불변 객체 예제 추가
    객체의 가변성에 따른 문제점

---
+++ corejavascript/immutable-object.js
@@ -0,0 +1,28 @@
+/**
+ * p.20 ~ 28 불변 객체
+ * 객체의 가변성에 따른 문제점
+ * 불변 객체 원칙을 위반, 객체의 속성을 변경 할 때는 새로운 객체를 생성하여 불변성을 유지
+ * 문제를 해결하기 위해서는 간단한 함수 선언(얕은 복사)과 라이브러리를 사용한다
+ */
+const champion = {
+    name: '이즈리얼',
+    position: '원딜',
+};
+
+const changePostion = function (champion, newPosition) {
+    // 동일한 메모리 주소를 참조, champion 의 속성도 변경이 일어남
+    const newChampion = champion;
+    newChampion.position = newPosition;
+    return newChampion;
+};
+
+const champion2 = changePostion(champion, '정글');
+
+// 두 참조가 같은 객체를 참조하고 있는지 확인
+if (champion === champion2) {
+    console.log('변경이 일어나지 않았습니다.');
+} else if (champion !== champion2) {
+    console.log('변경이 일어났습니다.');
+}
+
+console.log(champion, champion2);
