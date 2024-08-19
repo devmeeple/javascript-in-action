18b3e2e5679b22483a4d758df824fcf13d912817
미플 <devmeeple@gmail.com>
2024-08-19 19:20:52 +0900

    test: 빈 문자열 검증 리팩터링 테스트 구현

---
+++ test/javacan/password-strength-meter.spec.js
@@ -0,0 +1,39 @@
+const meter = (password) => {
+  if (!password?.trim()) {
+    return PasswordStrength.INVALID;
+  }
+
+  return PasswordStrength.VALID;
+};
+
+const PasswordStrength = {
+  VALID: '유효한 비밀번호 입니다.',
+  INVALID: '유효하지 않은 비밀번호 입니다.',
+};
+describe('PasswordStrengthMeterTest', () => {
+  it('유효한 비밀번호를 입력한다. [성공]', () => {
+    // given
+    const password = '1q2w3e4r';
+
+    // when
+    const sut = meter(password);
+
+    // then
+    expect(sut).toBe('유효한 비밀번호 입니다.');
+  });
+
+  describe('유효하지 않은 비밀번호를 입력하면 에러 메시지를 출력한다. [실패]', () => {
+    it.each([null, undefined, ''])(
+      '비밀번호 "%s"은(는) 유효하지 않은 비밀번호다.',
+      (invalidPassword) => {
+        // given
+
+        // when
+        const sut = meter(invalidPassword);
+
+        // then
+        expect(sut).toBe('유효하지 않은 비밀번호 입니다.');
+      },
+    );
+  });
+});
