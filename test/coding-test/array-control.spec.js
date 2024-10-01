dcf425a5517f9086454cfe74623f7f90da34c02c
미플 <devmeeple@gmail.com>
2024-10-02 07:59:38 +0900

    style(coding-test): 배열 제어하기 (1) 중복 제거하기

--- test/coding-test/array-control.spec.js
+++ test/coding-test/array-control.spec.js
@@ -1,25 +1,27 @@
 describe('배열 제어하기', () => {
-  it('[Set] 중복을 제거한다.', () => {
-    // given
-    const numbers = [4, 2, 2, 1, 3, 4];
+  describe('중복 제거하기', () => {
+    it('Set 자료구조를 사용해서 중복을 제거한다.', () => {
+      // given
+      const numbers = [4, 2, 2, 1, 3, 4];
 
-    // when
-    const sut = [...new Set(numbers)];
+      // when
+      const sut = [...new Set(numbers)];
 
-    // then
-    expect(sut).toEqual([4, 2, 1, 3]);
-  });
+      // then
+      expect(sut).toEqual([4, 2, 1, 3]);
+    });
 
-  it('filter + indexOf 조합으로 중복을 제거한다.', () => {
-    // given
-    const numbers = [4, 2, 2, 1, 3, 4];
+    it('filter + indexOf 조합으로 중복을 제거한다.', () => {
+      // given
+      const numbers = [4, 2, 2, 1, 3, 4];
 
-    // when
-    const sut = numbers.filter(
-      (value, index) => numbers.indexOf(value) === index,
-    );
+      // when
+      const sut = numbers.filter(
+        (value, index) => numbers.indexOf(value) === index,
+      );
 
-    // then
-    expect(sut).toEqual([4, 2, 1, 3]);
+      // then
+      expect(sut).toEqual([4, 2, 1, 3]);
+    });
   });
 });
